07c3d5502b1e5fee3beb13ccfa4f100ed2f312d7
Preston van Tonder <46090392+putquo@users.noreply.github.com>
2024-02-04 16:26:42 +0100

    refactor: move to unstable and add wezterm url

--- flake.nix
+++ flake.nix
@@ -1,25 +1,36 @@
 {
-  description = "My Nix Configuration";
+  description = "Nix-based configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    home-manager.url = "github:nix-community/home-manager/release-23.11";
+    home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";  
 
     nur.url = "github:nix-community/nur";
+
+    wezterm.url = "github:wez/wezterm?dir=nix&rev=5046fc225992db6ba2ef8812743fadfdfe4b184a";
+    wezterm.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, ... }@inputs: 
     let 
-      context = { ... }: rec {
-        user.name = "putquo";
-        user.description = "Preston van Tonder";
-        git.name = user.description;
+      context = rec {
+        user.identifier = "putquo";
+        user.name = "Preston van Tonder";
+        git.author = user.name;
         git.email = "46090392+putquo@users.noreply.github.com";
       };
 
-      overlays = [ inputs.nur.overlay ];
+      overlays = [
+        inputs.nur.overlay
+      ];
+
+      supportedSystems = [
+        "x86_64-linux"
+      ];
+      forAllSystems = with nixpkgs.lib; genAttrs supportedSystems;
+
     in {
       nixosConfigurations = {
         titan = import ./hosts/titan { inherit inputs context overlays; };
