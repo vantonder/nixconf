38d5f1e40931ebdcf2c9281db84b98e41a212f7c
Preston van Tonder <46090392+putquo@users.noreply.github.com>
2024-03-17 10:29:38 +0100

    feat: add development shell for nix

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";  
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     nixos-wsl.url = "github:nix-community/nixos-wsl";
     nixos-wsl.inputs.nixpkgs.follows = "nixpkgs";
@@ -13,23 +13,39 @@
     nur.url = "github:nix-community/nur";
   };
 
-  outputs = { self, ... }@inputs: 
-    let 
-      overlays = [
-        inputs.nur.overlay
-      ];
+  outputs = {self, ...} @ inputs: let
+    overlays = [
+      inputs.nur.overlay
+    ];
 
-      mkSystem = import ./lib/mksystem.nix { inherit inputs overlays; };
-    in {
-      nixosConfigurations = {
-        byte = mkSystem "byte" {
-          system = "x86_64-linux";
-          wsl = true;
-        };
+    mkSystem = import ./lib/mksystem.nix {inherit inputs overlays;};
 
-        titan = mkSystem "titan" {
-          system = "x86_64-linux";
-        };
+    supportedSystems = ["x86_64-linux"];
+    forEachSupportedSystem = f:
+      inputs.nixpkgs.lib.genAttrs supportedSystems (system:
+        f {
+          pkgs = import inputs.nixpkgs {inherit system;};
+        });
+  in {
+    nixosConfigurations = {
+      byte = mkSystem "byte" {
+        system = "x86_64-linux";
+        wsl = true;
+      };
+
+      titan = mkSystem "titan" {
+        system = "x86_64-linux";
       };
     };
+
+    devShells = forEachSupportedSystem ({pkgs}: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          deadnix
+          nixfmt
+          statix
+        ];
+      };
+    });
+  };
 }
